78ee220507322638b464667f48e42916b2b13ebd
Ahmed Attia <34924380+attia3@users.noreply.github.com>
2026-01-22 11:02:33 -0600

    Update Python environment path for reticulate

--- R_00_InstallingConda_Envi_Rgee.R
+++ R_00_InstallingConda_Envi_Rgee.R
@@ -3,11 +3,13 @@ install.packages(c("rgee","reticulate","geojsonio","V8","sf","terra"))
 install.packages(c("usethis", "devtools", "roxygen2", "testthat"))
 
 #- the correct order here 
+# Optional: manually point reticulate to the Python env used by rgee
 Sys.setenv(
-  RETICULATE_PYTHON = "C:/Users/ahmed.attia/AppData/Local/r-miniconda/envs/rgee311/python.exe"
+  RETICULATE_PYTHON = "~/AppData/Local/r-miniconda/envs/rgee_py311/python.exe"
 )
 library(reticulate)
 py_config()
+
 library(rgee)
 ee_Initialize()
 ee_Authenticate()
